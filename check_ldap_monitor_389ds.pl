df0293f58f5571e1a15b426ac3ab5e0c07276425
Paul Curie <paucur@worteks.com>
2019-04-17 11:46:26 +0200

    typo

--- check_ldap_monitor_389ds.pl
+++ check_ldap_monitor_389ds.pl
@@ -564,12 +564,12 @@ if ($errorcode) {
 #==========================================================================
 
 # Prepare PerfParse data
+my $perfparse = " ";
 if ($perf_data) {
     if ($warning == "") { $warning = 0; }
     if ($critical == "") { $critical = 0; }
     $perfparse = "|'$type'=" . $value . ";$warning;$critical";
 }
-
 
 # Test value and exit
 if ( $type =~ /^(version|currenttime|starttime|nbackends)$/) {
